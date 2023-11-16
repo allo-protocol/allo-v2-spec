406518e81a34403360e86833b11d994b1f531ac4
Aditya Anand M C <aditya.anandmc@gmail.com>
2023-11-17 03:21:31 +0530

    add auto indexed events (#28)

---
+++ .spec/migrations/1700161504000_create_distributed/down.sql
@@ -0,0 +1,5 @@
+BEGIN;
+    drop index public.idx_ig1upbfdlsjfchmxhrdhgp;
+    drop index public.idx_ipnkl2uh6ak1jmqe3buqxe;
+    drop table public.distributed;
+COMMIT;
---
+++ .spec/migrations/1700161504000_create_distributed/up.sql
@@ -0,0 +1,6 @@
+BEGIN;
+    create table public.distributed (recipient_id varchar, recipient_address varchar, amount varchar, sender varchar, contract_name varchar, contract_address varchar, transaction_hash varchar, log_index int4, block_hash varchar, block_number int8, block_timestamp timestamptz, chain_id varchar);
+    alter table public.distributed add constraint pk_wmjkmyft4jsezeqj5reuq9 primary key (transaction_hash, log_index);
+    create index idx_ipnkl2uh6ak1jmqe3buqxe on public.distributed (block_number, chain_id);
+    create index idx_ig1upbfdlsjfchmxhrdhgp on public.distributed (block_timestamp);
+COMMIT;
