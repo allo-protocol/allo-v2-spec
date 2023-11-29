adbc8b4f4f6892b4ed55d8fc4d1a0171b761dde9
Ben Whittle <benwhittle31@gmail.com>
2023-11-29 13:34:54 -0500

    tweak pg col types for massive numbers and snake_case

--- .spec/migrations/1701268192000_add_gov_hats_microgrant_column/up.sql
+++ .spec/migrations/1701268192000_add_gov_hats_microgrant_column/up.sql
@@ -1,7 +1,7 @@
 BEGIN;
     ALTER TABLE public.micro_grant ADD COLUMN gov varchar;
-    ALTER TABLE public.micro_grant ADD COLUMN snapshotReference int8;
-    ALTER TABLE public.micro_grant ADD COLUMN minVotePower varchar;
+    ALTER TABLE public.micro_grant ADD COLUMN snapshot_reference int8;
+    ALTER TABLE public.micro_grant ADD COLUMN min_vote_power varchar;
     ALTER TABLE public.micro_grant ADD COLUMN hats varchar;
-    ALTER TABLE public.micro_grant ADD COLUMN hatId bigint;
+    ALTER TABLE public.micro_grant ADD COLUMN hat_id varchar;
 COMMIT;
