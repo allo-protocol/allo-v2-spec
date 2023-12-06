afbb09b5c39513a17295ff4bf214ee2d650302ad
Ben Whittle <benwhittle31@gmail.com>
2023-12-05 20:25:14 -0600

    hard migrate to new spec event structure

--- .spec/migrations/1700161440000_create_allocated/up.sql
+++ .spec/migrations/1700161440000_create_allocated/up.sql
@@ -1,6 +1,6 @@
 BEGIN;
     create table public.allocated (recipient_id varchar, status varchar, sender varchar, contract_name varchar, contract_address varchar, transaction_hash varchar, log_index int4, block_hash varchar, block_number int8, block_timestamp timestamptz, chain_id varchar);
-    alter table public.allocated add constraint pk_aj9tf1zqrudmfm6lhjbxex primary key (transaction_hash, log_index);
+    alter table public.allocated add constraint pk_aj9tf1zqrudmfm6lhjbxex primary key (transaction_hash, log_index, chain_id);
     create index idx_wxeewmbofi3ugmxszruupy on public.allocated (block_number, chain_id);
     create index idx_7ddunrdjfzvd9kgawjf51h on public.allocated (block_timestamp);
     COMMENT ON TABLE public.allocated IS E'@foreignKey (contract_address, chain_id) references public.micro_grant (strategy, chain_id)|@fieldName microGrant|@foreignFieldName allocateds';
