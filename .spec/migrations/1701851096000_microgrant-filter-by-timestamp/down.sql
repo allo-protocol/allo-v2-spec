1066eb320cca18b6da10a9c3a6f7088f8bf58913
Aditya Anand M C <aditya.anandmc@gmail.com>
2023-12-06 14:44:00 +0530

    update

--- .spec/migrations/1701851096000_microgrant-filter-by-timestamp/down.sql
+++ .spec/migrations/1701851096000_microgrant-filter-by-timestamp/down.sql
@@ -1,8 +1,8 @@
 -- Drop function to filter microGrants before allocationStartTime
-DROP FUNCTION IF EXISTS public.micro_grants_before_start_time(start_time int4);
+DROP FUNCTION IF EXISTS public.upcoming_micro_grants(start_time int4);
 
 -- Drop function to filter microGrants after allocationEndTime
-DROP FUNCTION IF EXISTS public.micro_grants_after_end_time(end_time int4);
+DROP FUNCTION IF EXISTS public.ended_micro_grants(end_time int4);
 
 -- Drop function to filter microGrants between allocationStartTime and allocationEndTime
-DROP FUNCTION IF EXISTS public.micro_grants_between_times(start_time int4, end_time int4);
+DROP FUNCTION IF EXISTS public.active_micro_grants(start_time int4, end_time int4);
--- .spec/migrations/1701851096000_microgrant-filter-by-timestamp/up.sql
+++ .spec/migrations/1701851096000_microgrant-filter-by-timestamp/up.sql
@@ -1,29 +1,29 @@
 -- Function to filter microGrants before allocationStartTime
-CREATE OR REPLACE FUNCTION public.micro_grants_before_start_time(start_time int4)
+CREATE OR REPLACE FUNCTION public.upcoming_micro_grants(start_time int4)
 RETURNS SETOF public.micro_grant AS $$
     SELECT * FROM public.micro_grant
     WHERE allocation_start_time < start_time;
 $$ LANGUAGE sql STABLE;
 
-COMMENT ON FUNCTION micro_grants_before_start_time(start_time int4) IS E'@filterable\n@sortable';
+COMMENT ON FUNCTION upcoming_micro_grants(start_time int4) IS E'@filterable\n@sortable';
 
 
 -- Function to filter microGrants after allocationEndTime
-CREATE OR REPLACE FUNCTION public.micro_grants_after_end_time(end_time int4)
+CREATE OR REPLACE FUNCTION public.ended_micro_grants(end_time int4)
 RETURNS SETOF public.micro_grant AS $$
     SELECT * FROM public.micro_grant
     WHERE allocation_end_time > end_time;
 $$ LANGUAGE sql STABLE;
 
-COMMENT ON FUNCTION micro_grants_after_end_time(end_time int4) IS E'@filterable\n@sortable';
+COMMENT ON FUNCTION ended_micro_grants(end_time int4) IS E'@filterable\n@sortable';
 
 
 -- Function to filter microGrants between allocationStartTime and allocationEndTime
-CREATE OR REPLACE FUNCTION public.micro_grants_between_times(start_time int4, end_time int4)
+CREATE OR REPLACE FUNCTION public.active_micro_grants(start_time int4, end_time int4)
 RETURNS SETOF public.micro_grant AS $$
     SELECT * FROM public.micro_grant
     WHERE allocation_start_time >= start_time
         AND allocation_end_time <= end_time;
 $$ LANGUAGE sql STABLE;
 
-COMMENT ON FUNCTION micro_grants_between_times(start_time int4, end_time int4) IS E'@filterable\n@sortable';
+COMMENT ON FUNCTION active_micro_grants(start_time int4, end_time int4) IS E'@filterable\n@sortable';
